679dcff85769495ffd1b05a8eaa625f19804f1f3
M-Sancier <msancier@expertime.com>
2026-07-17 16:52:05 +0200

    Update button text from 'Go to PowerAppsCodeApps Repo !!!' to 'Let’s Go !!!'

--- src/App.tsx
+++ src/App.tsx
@@ -127,7 +127,7 @@ const App: React.FC = () => {
                 className={styles.button}
                 onClick={() => window.open('https://aka.ms/codeapps', '_blank')}
               >
-                Go to PowerAppsCodeApps Repo !!!
+                Let’s Go !!!
               </Button>
             </div>
           </Card>
